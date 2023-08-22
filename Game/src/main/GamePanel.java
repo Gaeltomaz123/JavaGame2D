5268f41a9c6d8c1a492df1ab8b74ada2a6004425
Gaeltomaz123 <gaeltomaz@gmail.com>
2023-08-22 17:24:01 -0300

    Create a loop with a random color change to the rectangle

--- Game/src/main/GamePanel.java
+++ Game/src/main/GamePanel.java
@@ -1,17 +1,21 @@
 package main;
 
 import javax.swing.JPanel;
-
 import inputs.Keyboard;
 import inputs.Mouse;
-
+import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 
-public class GamePanel extends JPanel{
+public class GamePanel extends JPanel {
     private Mouse mouse;
-    private int xDelta=100, yDelta=100;
+    private float xDelta=100, yDelta=100, xDir=0.1f, yDir=0.1f;
+    private Color color;
+    private Random random;
 
     public GamePanel() {
+        random = new Random();
+        color = new Color(255, 0, 0);
         mouse = new Mouse(this);
         addKeyListener(new Keyboard(this));
         addMouseListener(mouse);
@@ -20,22 +24,42 @@ public class GamePanel extends JPanel{
 
     public void changeX(int speed) {
         this.xDelta += speed;
-        repaint();
     }
 
     public void changeY(int speed) {
         this.yDelta += speed;
-        repaint();
     }
 
     public void changePos(int x, int y){
         this.xDelta = x;
         this.yDelta = y;
-        repaint();
     }
 
     public void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
-        graphics.fillRect(xDelta, yDelta, 300, 100);
+        updateRect();
+        graphics.setColor(color);
+        graphics.fillRect((int)xDelta, (int)yDelta, 300, 100);
+        repaint();
+    }
+
+    private void updateRect() {
+        xDelta += xDir;
+        if(xDelta > 800 || xDelta < 0){
+            xDir *= -1;
+            color = rndColor();
+        }
+        yDelta += yDir;
+        if(yDelta > 600 || yDelta < 0){
+            yDir *= -1;
+            color = rndColor();
+        }
+    }
+
+    private Color rndColor() {
+        int r=random.nextInt(256);
+        int g=random.nextInt(256);
+        int b=random.nextInt(256);
+        return new Color(r, g, b);
     }
 }
